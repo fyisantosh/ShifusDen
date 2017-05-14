90b57dc215ec3fef45009aedb7d9a646d3cfd7f6
SantoshJ <fyisantosh@gmail.com>
2017-05-13 21:31:19 -0400

    fixed start value to 1

--- shifu/node/app.js
+++ shifu/node/app.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.get("/trainings", function (req, res) {
     console.log("Getting " + req.query.n + " trainings starting from " + req.query.s + " with term " + req.query.q);
     n = req.query.n || 10;
-    s = req.query.s || 0;
+    s = req.query.s || 1;
     q = req.query.q || '';
     res.send(trainingsDAO.getTrainingsList(n,s,q));
 });
