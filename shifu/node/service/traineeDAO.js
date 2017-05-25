8b70d285535235f6a05c36ec68b228987d47203a
Santosh J <fyisantosh@gmail.com>
2017-05-25 15:28:53 -0400

    Fixed API to better names

--- shifu/node/service/traineeDAO.js
+++ shifu/node/service/traineeDAO.js
@@ -7,67 +7,61 @@ var async = require("async");
 
 var traineeDAO = {
     getAll: function (req, res) {
-        q = req.query.q || '';
+        f = req.query.f || ''; //Only first name
+        l = req.query.l || ''; //Only last name
+        p = req.query.p || ''; //Only first name
+        wt = req.query.wt || 0; //With training details, if 1
+
         var query = trainee.find(
             {
-                $or: [
-                    { 'name.first': { '$regex': q, '$options': 'i' } },
-                    { 'name.last': { '$regex': q, '$options': 'i' } },
-                    { '_id': { '$regex': q, '$options': 'i' } }]
+                $and: [
+                    { 'name.first': { '$regex': f, '$options': 'i' } },
+                    { 'name.last': { '$regex': l, '$options': 'i' } },
+                    { '_id': { '$regex': p, '$options': 'i' } }]
             }
         );
 
-        query.exec(function (err, ts) {
-            if (err) throw err;
-            res.send(ts);
-        });
+        if (wt == 1) {
+            query.lean().exec(function (err, t1) {
+                total_tn = t1.length - 1;
+
+                async.whilst(
+                    function () {
+                        return total_tn >= 0;
+                    },
+                    function (callback) {
+                        curr_tn = t1[total_tn]._id;
+                        tr_q = training.findOne(
+                            { "trainees": { $elemMatch: { "status": "a", "psno": curr_tn } } },
+                            { "tname": 1, _id: 0 }
+                        );
+
+                        tr_q.lean().exec(function (er, tg1) {
+                            if (tg1 != null) {
+                                t1[total_tn].active_training = tg1.tname;
+                            }
+                            total_tn--;
+                            callback(null, total_tn);
+                        });
+                    },
+                    function (err, n) {
+                        res.send(t1);
+                    }
+                );
+
+            });
+        } else {
+            query.exec(function (err, ts) {
+                if (err) throw err;
+                res.send(ts);
+            });
+        }
     },
 
     getById: function (req, res) {
         trainee.find({ '_id': req.params['id'] }, function (err, ts) {
             if (err) throw err;
             res.send(ts);
-        });
-    },
-
-    getAllWithActiveTraining: function (req, res) {
-        q = req.query.q || '';
-        var query = trainee.find(
-            {
-                $or: [
-                    { 'name.first': { '$regex': q, '$options': 'i' } },
-                    { 'name.last': { '$regex': q, '$options': 'i' } },
-                    { '_id': { '$regex': q, '$options': 'i' } }]
-            }
-        );
-
-        query.lean().exec(function (err, t1) {
-            total_tn = t1.length - 1;
-
-            async.whilst(
-                function () {
-                    return total_tn >= 0;
-                },
-                function (callback) {
-                    curr_tn = t1[total_tn]._id;
-                    tr_q = training.findOne(
-                        { "trainees": { $elemMatch: { "status": "a", "psno": curr_tn } } },
-                        { "tname": 1, _id: 0 }
-                    );
-
-                    tr_q.lean().exec(function (er, tg1) {
-                        if (tg1 != null) {
-                            t1[total_tn].active_training = tg1.tname;
-                        }
-                        total_tn--;
-                        callback(null, total_tn);
-                    });
-                },
-                function (err, n) {                    
-                    res.send(t1);
-                }
-            );
-
         });
     }
 }
